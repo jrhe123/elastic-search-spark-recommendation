6dde883ef339ad564759de241eb7453fadaff1f7
jrhe123 <keith_he@outlook.com>
2025-03-22 18:34:54 -0400

    Add thymeleaf template

--- src/main/java/com/he/rating/controller/UserController.java
+++ src/main/java/com/he/rating/controller/UserController.java
@@ -8,6 +8,7 @@ import com.he.rating.model.UserModel;
 import com.he.rating.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(value ="user")
@@ -15,6 +16,14 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        String name = "roytest";
+        ModelAndView mav = new ModelAndView("/index.html");
+        mav.addObject("name", name);
+        return mav;
+    }
 
     @GetMapping("/test")
     public String test(){
