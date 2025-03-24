25e9bd932dfc89000d1f282505deaefe1b9d4fc6
jrhe123 <keith_he@outlook.com>
2025-03-24 19:14:44 -0400

    Add shop table

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -32,3 +32,20 @@ CREATE TABLE `rating`.`category` (
                                      UNIQUE INDEX `name_unique_index` USING BTREE (`name`) VISIBLE);
 
 
+CREATE TABLE `rating`.`shop` (
+                                 `id` INT NOT NULL AUTO_INCREMENT,
+                                 `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                 `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                 `name` VARCHAR(80) NOT NULL DEFAULT '',
+                                 `remark_score` DECIMAL(2,1) NOT NULL DEFAULT 0,
+                                 `price_per_man` INT NOT NULL DEFAULT 0,
+                                 `latitude` DECIMAL(10,6) NOT NULL DEFAULT 0,
+                                 `longitude` DECIMAL(10,6) NOT NULL DEFAULT 0,
+                                 `category_id` INT NOT NULL DEFAULT 0,
+                                 `tags` VARCHAR(2000) NOT NULL DEFAULT '',
+                                 `start_time` VARCHAR(200) NOT NULL DEFAULT '',
+                                 `end_time` VARCHAR(200) NOT NULL DEFAULT '',
+                                 `address` VARCHAR(200) NOT NULL DEFAULT '',
+                                 `seller_id` INT NOT NULL DEFAULT 0,
+                                 `icon_url` VARCHAR(100) NOT NULL DEFAULT '',
+                                 PRIMARY KEY (`id`));
