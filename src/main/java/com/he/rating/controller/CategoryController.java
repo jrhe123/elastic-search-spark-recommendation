ed5a90085c870c03ade8de152dba54683ccb93fa
jrhe123 <keith_he@outlook.com>
2025-03-24 17:35:21 -0400

    Add mobile app category controller

---
+++ src/main/java/com/he/rating/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.he.rating.controller;
+
+import com.he.rating.common.CommonRes;
+import com.he.rating.model.CategoryModel;
+import com.he.rating.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @RequestMapping("/list")
+    public CommonRes list() {
+        List<CategoryModel> categoryModels = categoryService.selectAll();
+        return CommonRes.create(categoryModels);
+    }
+}
