6d893bc80c6c84e3559488efc86015221825dbc8
jrhe123 <keith_he@outlook.com>
2025-03-24 16:03:01 -0400

    Add category table

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -19,3 +19,16 @@ CREATE TABLE `rating`.`seller` (
                                       `remark_score` DECIMAL(2,1) NOT NULL DEFAULT 0,
                                       `disabled_flag` INT NOT NULL DEFAULT 0,
                                       PRIMARY KEY (`id`));
+
+
+CREATE TABLE `rating`.`category` (
+                                     `id` INT NOT NULL AUTO_INCREMENT,
+                                     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                     `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                     `name` VARCHAR(20) NOT NULL DEFAULT '',
+                                     `icon_url` VARCHAR(200) NOT NULL DEFAULT '',
+                                     `sort` INT NOT NULL DEFAULT 0,
+                                     PRIMARY KEY (`id`),
+                                     UNIQUE INDEX `name_unique_index` USING BTREE (`name`) VISIBLE);
+
+
